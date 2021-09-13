aa3b83453209afc19fe3c72af7471fa3eb61b170
vijayskk <vijayskk007@gmail.com>
2021-09-13 19:25:56 +0530

    darkmode removed due to issues

--- src/App.js
+++ src/App.js
@@ -9,26 +9,9 @@ import Post from './components/Post';
 import Home from './components/Home';
 import Login from './components/Login';
 import Profile from './components/Profile';
-import Darkmode from 'darkmode-js';
 
 
 function App() {
-  const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#000000',  // default: '#000000'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌙', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
   return (
     <div className="App">
       <Router>
